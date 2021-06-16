f1daffc2a27764080a13a5e9de8297bb2af1951e
tapuks <david_berdiell@hotmail.com>
2021-06-16 17:32:25 +0000

    token

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planets, People
 #from models import Person
 
 app = Flask(__name__)
@@ -20,6 +20,8 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
+
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -30,14 +32,104 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+# @app.route('/user', methods=['GET'])
+# def handle_hello():
+#     # users = User.query.all()
+#     # print(users)
+#     # users=[] 
+#     # for user in users:
+#     #     array.append(user.serialize())
+#     # print(array)
+#     # return jsonify({"users":array})
+#     return jsonify({"users": users}),200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/people', methods=['GET'])
+def show_people():
+    people = People.query.all()
+    people_all = []
+    for person in people:
+        people_all.append( person.serialize())
+    return jsonify({"people" : people_all}),200
 
-    return jsonify(response_body), 200
+@app.route('/people/<int:people_id>', methods=['GET'])
+def show_one_people(people_id):
+    person = People.query.filter_by(id = people_id).first()
+    return jsonify(person.serialize()),200
+
+@app.route('/people/<int:people_id>', methods=['DELETE'])
+def delete_one_people(people_id):
+    user = People.query.get(people_id)
+    db.session.delete(user)
+    db.session.commit()
+    # show all
+    people = People.query.all()
+    people_all = []
+    for person in people:
+        people_all.append( person.serialize())
+    return jsonify({"people" : people_all}),200
+
+   
+@app.route('/planets', methods=['GET'])
+def show_all_planets():
+    planets = Planets.query.all()
+    planets_all = []
+    for planet in planets:
+        planets_all.append(planet.serialize())
+    return jsonify({"planets":planets_all}),200
+
+@app.route('/planet/<int:planet_id>', methods=['GET'])
+def show_planet(planet_id):
+    planet = Planets.query.filter_by(id=planet_id).first()
+    serialize=planet.serialize()
+    return jsonify(serialize),200
+
+@app.route('/login', methods=['POST'])
+def handle_login():
+    print("@@@")
+    json = request.get_json()
+
+    # si no nos manda un json 
+    if json is None:
+        raise APIException("You should send json body")
+    if "email" not in json or "password" not in json:
+        raise APIException("Email and password are required")
+
+    email = json["email"]
+    password = json["password"]
+
+    # Pregunta a la base de datos si tenemos un user con ese email y contraseña
+    user = User.get_with_login_credentials(email, password)
+    # print(user.serialize())
+    if user is None:
+        return "error", 306
+        # raise APIException("User not found")
+
+    token = "222edse2¡rwter7545teji9o"
+    user.assign_token(token)
+    print(user.serialize())
+    #  devolvemos un usuario al frond
+    return jsonify({"user": user.serialize(), "token": token}), 200
+
+@app.route('/profile', methods=['POST'])
+def handle_profile():
+    json = request.get_json()
+    token = json[token]
+    user = User.get_with_token(token)
+    if user is None: 
+        raise APIException("Invalid token")
+    return jsonify(user.serialize()), 200
+
+
+
+# @app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
+# def add_favourite_planet(planet_id):
+#     body = request.get_json()
+#     # print(body)
+#     # print(planet_id)
+#     favorites.append(body)
+#     return jsonify({"favorites":favorites}),200
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -7,9 +7,21 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    token = db.Column(db.String(120), unique=False, nullable=True)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    @staticmethod
+    def get_with_login_credentials(email, password):
+        return User.query.filter_by(email=email).filter_by(password=password).first()
+
+    # como primer parametro se pasa self, pero a la hora de llamar la funcion no se pone
+    def assign_token(self,token):
+        self.token = token
+        db.session.add(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_with_token(token):
+        return User.query.filter_by(token=token).first()
 
     def serialize(self):
         return {
@@ -17,3 +29,39 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Planets(db.Model):
+    __tablename__="Planets"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    population = db.Column(db.String(120), unique=False, nullable=False)
+    terrain = db.Column(db.String(120), unique=False, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "population": self.population,
+            "terrain": self.terrain
+        }
+
+        
+class People(db.Model):
+    __tablename__="People"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=False, nullable=False)
+    eye_color = db.Column(db.String(25), unique=False, nullable=False)
+    gender = db.Column(db.String(25), unique=False, nullable=False)
+    hair_color = db.Column(db.String(25), unique=False, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "eye_color": self.eye_color,
+            "gender": self.gender,
+            "hair_color": self.hair_color,
+            # do not serialize the password, its a security breach
+        }
+    
+
